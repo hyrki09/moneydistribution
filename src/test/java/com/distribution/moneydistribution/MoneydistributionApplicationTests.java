910a016eedc50ab59a2ade6c5ef4f0620654cd8c
hyrki09 <hyrki0901@naver.com>
2024-07-14 20:43:12 +0900

    first-commit

---
+++ src/test/java/com/distribution/moneydistribution/MoneydistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distribution.moneydistribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneydistributionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
