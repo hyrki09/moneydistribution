d3f6a90a0ebafd55cc3834bca92dd3fb41f68b77
hyrki0901 <hyrki0901@naver.com>
2024-08-01 17:57:48 +0900

    로그인 페이지 - 2

--- src/main/java/com/distribution/moneydistribution/login/LoginController.java
+++ src/main/java/com/distribution/moneydistribution/login/LoginController.java
@@ -3,7 +3,9 @@ package com.distribution.moneydistribution.login;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/login")
@@ -13,9 +15,24 @@ public class LoginController {
 
 
     @GetMapping("")
-    public String login() {
+    public String loginPage() {
+        log.info("login 통과");
 
-        log.debug("login 통과");
+        // 현재 로그인 세션이 남아있는지 확인 필요
+
+
+
+        return "login/loginpage";
+    }
+
+    @PostMapping("")
+    public String login(
+            @RequestParam String id,
+            @RequestParam String password
+    ){
+        log.info("로그인 시도");
+        log.info("id : {}", id);
+        log.info("password : {}", password);
 
 
         return "login/loginpage";
